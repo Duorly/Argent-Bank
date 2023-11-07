23041d2b440ea85a8fdcb20f0150aa23d6d22681
Duorly <nebel.mass@gmail.com>
2023-11-07 13:27:26 +0100

    Add user transaction component and profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,13 @@
-export const Profile = () => {
+import { Transaction } from "@/components/Transaction";
+import "./style.scss";
+
+export const Profile = (): JSX.Element => {
     return (
-        <div>
-            Profile
-        </div>
-    )
+        <main className="bg-dark">
+            <h2 className="sr-only">Accounts</h2>
+            <Transaction title={"Argent Bank Checking (x8349)"} amount={"2,082.79"} />
+            <Transaction title={"Argent Bank Savings (x6712)"} amount={"10,928.42"} />
+            <Transaction title={"Argent Bank Credit Card (x8349)"} amount={"184.30"} />
+        </main>
+    );
 }
